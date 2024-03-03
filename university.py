df6f85121516e4ebc92a349be2f3ad89c0a73e53
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-03-03 17:37:02 +0700

    Add files via upload
    Stage 6/7

--- university.py
+++ university.py
@@ -1,22 +1,23 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass(eq=True, order=True, frozen=True)
 class Applicant:
     first_name: str
     last_name: str
-    gpa: float
+    results: dict[str: int] = field(hash=False)
     priority: tuple[str, str, str]
 
 
 class Admission:
     def __init__(self):
         self.file = 'applicants.txt'
-        self.departments_acceptance: dict[str: int] = {'Biotech': 0,
-                                                       'Chemistry': 0,
-                                                       'Engineering': 0,
-                                                       'Mathematics': 0,
-                                                       'Physics': 0}
+        self.departments_acceptance: dict[str: dict[str: str, str: int]] = \
+            {'Biotech': {'subject': 'Chemistry', 'max_num': 0},
+             'Chemistry': {'subject': 'Chemistry', 'max_num': 0},
+             'Engineering': {'subject': 'Computer science', 'max_num': 0},
+             'Mathematics': {'subject': 'Math', 'max_num': 0},
+             'Physics': {'subject': 'Physics', 'max_num': 0}}
         self.applicants: list[Applicant] = []
         self.selected: dict[str: list[Applicant]] = {'Biotech': [],
                                                      'Chemistry': [],
@@ -33,19 +34,26 @@ class Admission:
                 print('Invalid number')
             else:
                 for department in self.departments_acceptance:
-                    self.departments_acceptance[department] = self.max_accepted
+                    self.departments_acceptance[department]['max_num'] = self.max_accepted
                 break
 
     def load_applicants(self) -> None:
         with open(self.file) as file:
             for line in file:
                 data = line.strip().split()
-                self.applicants.append(Applicant(data[0], data[1], float(data[2]), (data[3], data[4], data[5])))
+                self.applicants.append(Applicant(data[0], data[1],
+                                                 {'Physics': int(data[2]),
+                                                  'Chemistry': int(data[3]),
+                                                  'Math': int(data[4]),
+                                                  'Computer science': int(data[5])},
+                                                 (data[6], data[7], data[8])))
 
     def admission(self) -> None:
         applicants = set(self.applicants)
         for preferred in range(3):  # preferred department  0, then 1, then 2
-            for department, max_num in self.departments_acceptance.items():
+            for department in self.departments_acceptance.keys():
+                # subject = self.departments_acceptance[department]['subject']
+                max_num = self.departments_acceptance[department]['max_num']
                 accepted_num = len(self.selected[department])  # already accepted applicants
                 if accepted_num >= max_num:  # already accepted max number?
                     continue
@@ -57,21 +65,26 @@ class Admission:
                 applicants -= set(selected_applicants)  # remove the accepted applicants
 
         for department in self.selected:  # final sorting
-            self.selected[department].sort(key=lambda x: (-x.gpa, x.first_name, x.last_name))
+            self.selected[department].sort(
+                key=lambda x: (-x.results[self.departments_acceptance[department]['subject']],
+                               x.first_name, x.last_name))
 
-    @staticmethod
-    def sort_applicants(department: str,
+    def sort_applicants(self, department: str,
                         applicants: list[Applicant] | set[Applicant], preferred: int) -> list[Applicant]:
 
         # new set of applicants who chose this department as preferred(0 or 1 or 2)
         department_applicants = {x for x in applicants if x.priority[preferred] == department}
-        return sorted(department_applicants, key=lambda x: (-x.gpa, x.first_name, x.last_name))
+        return sorted(department_applicants,
+                      key=lambda x: (-x.results[self.departments_acceptance[department]['subject']],
+                                     x.first_name,
+                                     x.last_name))
 
     def print_admission_result(self):
         for department, applicants in self.selected.items():
             print('\n', department)
             for applicant in applicants:
-                print(applicant.first_name, applicant.last_name, applicant.gpa)
+                print(applicant.first_name, applicant.last_name,
+                      applicant.results[self.departments_acceptance[department]['subject']])
 
     def start(self) -> None:
         self.load_applicants()
