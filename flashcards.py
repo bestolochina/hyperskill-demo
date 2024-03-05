fc0374ec4c9906828e655d0cc637ad0057f543ec
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-03-06 00:25:01 +0700

    Add files via upload
    5/7

--- flashcards.py
+++ flashcards.py
@@ -1,50 +1,25 @@
 from dataclasses import dataclass
+from random import choices
+import shelve
+import sys
 
 
 @dataclass
 class Card:
-    number: int
     term: str
     definition: str
 
 
 class Game:
     def __init__(self):
-        self.number: int = 0
         self.cards: list[Card] = []
+        self.file: str = 'flashcards'
 
-    def set_number(self) -> None:
-        while True:
-            try:
-                self.number = int(input('Input the number of cards:\n'))
-                break
-            except ValueError:
-                print('Invalid number')
+    def terms(self) -> list[str]:
+        return [card.term for card in self.cards]
 
-    def set_cards(self) -> None:
-        for number in range(1, self.number + 1):
-
-            print(f'The term for card #{number}:')
-            while True:
-                term = input()
-                if term not in self.terms():
-                    break
-                print(f'The term "{term}" already exists. Try again:')
-
-            print(f'The definition for card #{number}:')
-            while True:
-                definition = input()
-                if definition not in self.definitions():
-                    break
-                print(f'The definition "{definition}" already exists. Try again:')
-
-            self.cards.append(Card(number, term, definition))
-
-    def terms(self) -> set[str]:
-        return {card.term for card in self.cards}
-
-    def definitions(self) -> set[str]:
-        return {card.definition for card in self.cards}
+    def definitions(self) -> list[str]:
+        return [card.definition for card in self.cards]
 
     def get_term(self, definition: str) -> str | bool:
         for card in self.cards:
@@ -52,8 +27,62 @@ class Game:
                 return card.term
         return False
 
-    def main_cycle(self) -> None:
-        for card in self.cards:
+    @staticmethod
+    def choose(options: list | str,
+               prompt: str = '\nInput the action (add, remove, import, export, ask, exit):\n',
+               err: str = 'Incorrect command') -> int | str:
+        while True:
+            if options == 'int':
+                try:
+                    number = int(input(prompt).strip())
+                    if number > 0:
+                        return number
+                    raise ValueError
+                except ValueError:
+                    pass
+            elif isinstance(options, list):
+                choice = input(prompt).strip().lower()
+                if choice in options:
+                    return choice
+            print(err)
+
+    def add(self) -> None:
+        print('The card:')
+        while True:
+            term = input()
+            if term not in self.terms():
+                break
+            print(f'The card "{term}" already exists. Try again:')
+
+        print('The definition of the card:')
+        while True:
+            definition = input()
+            if definition not in self.definitions():
+                break
+            print(f'The definition "{definition}" already exists. Try again:')
+
+        self.cards.append(Card(term, definition))
+        print(f'The pair ("{term}":"{definition}") has been added.')
+
+    def remove(self):
+        card = input('Which card?\n')
+        cards = self.terms()
+        if card in cards:
+            self.cards.pop(cards.index(card))
+            print('The card has been removed.')
+        else:
+            print(f'Can\'t remove "{card}": there is no such card.')
+
+    def import_(self):
+        pass
+
+    def export(self):
+        pass
+
+    def ask(self) -> None:
+        num = self.choose('int', 'How many times to ask?', 'Invalid number')
+        cards = choices(self.cards, k=num)
+        for card in cards:
             user_definition = input(f'Print the definition of "{card.term}":\n')
             if user_definition == card.definition:
                 print('Correct!')
@@ -63,15 +92,29 @@ class Game:
             else:
                 print(f'Wrong. The right answer is "{card.definition}".')
 
-    def start(self) -> None:
-        self.set_number()
-        self.set_cards()
-        self.main_cycle()
+    @staticmethod
+    def exit():
+        print('Bye bye!')
+        sys.exit()
+
+    def main_menu(self) -> None:
+        while True:
+            choice = self.choose(['add', 'remove', 'import', 'export', 'ask', 'exit'])
+            if choice == 'add':
+                self.add()
+            elif choice == 'remove':
+                self.remove()
+            elif choice == 'import':
+                self.import_()
+            elif choice == 'export':
+                self.export()
+            elif choice == 'exit':
+                self.exit()
 
 
 def main() -> None:
     game = Game()
-    game.start()
+    game.main_menu()
 
 
 if __name__ == '__main__':
