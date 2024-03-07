0630978ef762d495a97fe2ceaf00b3f781050957
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-03-07 13:24:03 +0700

    Add files via upload
    stage 5/7 - file not found error

--- flashcards.py
+++ flashcards.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from dacite import from_dict
 from random import choices
 import json
@@ -13,22 +13,21 @@ class Card:
 
 @dataclass
 class Cards:
-    cards: list[Card]
+    my_cards: list[Card]
 
 
 class Game:
     def __init__(self):
-        self.cards: list[Card] = []
-        self.file: str = 'flashcards'
+        self.cards: Cards = Cards([])
 
     def terms(self) -> list[str]:
-        return [card.term for card in self.cards]
+        return [card.term for card in self.cards.my_cards]
 
     def definitions(self) -> list[str]:
-        return [card.definition for card in self.cards]
+        return [card.definition for card in self.cards.my_cards]
 
     def get_term(self, definition: str) -> str | bool:
-        for card in self.cards:
+        for card in self.cards.my_cards:
             if card.definition == definition:
                 return card.term
         return False
@@ -67,27 +66,47 @@ class Game:
                 break
             print(f'The definition "{definition}" already exists. Try again:')
 
-        self.cards.append(Card(term, definition))
+        self.cards.my_cards.append(Card(term, definition))
         print(f'The pair ("{term}":"{definition}") has been added.')
 
     def remove(self):
         card = input('Which card?\n')
         cards = self.terms()
         if card in cards:
-            self.cards.pop(cards.index(card))
+            self.cards.my_cards.pop(cards.index(card))
             print('The card has been removed.')
         else:
             print(f'Can\'t remove "{card}": there is no such card.')
 
     def import_(self):
-        pass
+        file_name = input('File name:\n')
+        with open(file_name) as file:
+            import_dict = json.load(file)
+        print(import_dict)
+        print(asdict(self.cards))
+        for card in import_dict['my_cards']:
+            if card['term'] not in self.terms() and card['definition'] in self.definitions():
+                continue
+            elif card['term'] not in self.terms():
+                self.cards.my_cards.append(Card(card['term'], card['definition']))
+            else:
+                idx = self.terms().index(card['term'])
+                self.cards.my_cards[idx] = Card(card['term'], card['definition'])
+
+        print(asdict(self.cards))
+        length = len(import_dict['my_cards'])
+        print(f'{length} cards have been loaded.')
 
     def export(self):
-        pass
+        file_name = input('File name:\n')
+        length = len(self.cards.my_cards)
+        with open(file_name, 'w', encoding='UTF-8') as file:
+            json.dump(asdict(self.cards), file)
+        print(f'{length} cards have been saved.')
 
     def ask(self) -> None:
         num = self.choose('int', 'How many times to ask?', 'Invalid number')
-        cards = choices(self.cards, k=num)
+        cards = choices(self.cards.my_cards, k=num)
         for card in cards:
             user_definition = input(f'Print the definition of "{card.term}":\n')
             if user_definition == card.definition:
@@ -114,6 +133,8 @@ class Game:
                 self.import_()
             elif choice == 'export':
                 self.export()
+            elif choice == 'ask':
+                self.ask()
             elif choice == 'exit':
                 self.exit()
 
