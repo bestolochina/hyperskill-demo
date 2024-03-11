57c48251b631b40002bf901c6cb0540984060a7d
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-03-12 01:26:52 +0700

    Add files via upload
    Stage 6/6

--- game.py
+++ game.py
@@ -9,11 +9,11 @@ class KnightTourPuzzle:
         self.starting_position: tuple[int, int] = self.input_2_nums(dimensions=False,
                                                                     prompt="Enter the knight's starting position: ",
                                                                     err='Invalid dimensions!')
-        self.current_position: tuple[int, int] = self.starting_position
         self.board: list[list[str | int]] = \
-            [[0 for x in range(self.board_dimensions[1])] for y in range(self.board_dimensions[0])]
+            [['_' for x in range(self.board_dimensions[1])] for y in range(self.board_dimensions[0])]
         self.board[self.starting_position[0]][self.starting_position[1]] = 'X'
         self.moves: tuple = ((2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1))
+        self.visited_squares: int = 1
 
     def input_2_nums(self, dimensions: bool = False,
                      prompt: str = 'Enter your next move: ',
@@ -40,19 +40,19 @@ class KnightTourPuzzle:
             return True
         return False
 
-    def find_possible_moves(self, y: int, x: int, board: list[list[int | str]],
-                            depth: int = 1) -> tuple[list[list[int | str]], set[tuple[int, int]]] | None:
-        if depth < 0:
-            return
-
-        possible_moves = set()
+    def possible_moves(self, y: int, x: int, board: list[list[int | str]]) -> set[tuple[int, int]]:
+        moves = set()
         for move in self.moves:
             new_y, new_x = y + move[0], x + move[1]
-            if self.within_borders(new_y, new_x) and board[new_y][new_x] not in {'*', 'X'}:
-                possible_moves.add((new_y, new_x))
-                if board[y][x] not in {'*', 'X'}:
-                    board[y][x] += 1
-                self.find_possible_moves(new_y, new_x, board, depth - 1)
+            if self.within_borders(new_y, new_x) and board[new_y][new_x] == '_':
+                moves.add((new_y, new_x))
+        return moves
+
+    def get_board_and_moves(self, y: int, x: int,
+                            board: list[list[int | str]]) -> tuple[list[list[int | str]], set[tuple[int, int]]]:
+        possible_moves = self.possible_moves(y, x, board)
+        for new_y, new_x in possible_moves:
+            board[new_y][new_x] = len(self.possible_moves(new_y, new_x, board))
         return board, possible_moves
 
     @staticmethod
@@ -67,10 +67,10 @@ class KnightTourPuzzle:
         for y in range(dimension_y - 1, -1, -1):
             print(f'{y + 1:>{left_width}}| ', end='')  # Y coordinates
             for x in range(dimension_x):
-                if board[y][x] == 0:
-                    print('_' * cell_width + ' ', end='')
+                if board[y][x] == '_':
+                    print('_' * cell_width + ' ', end='')  # empty cell
                 else:
-                    print(' ' * (cell_width - 1) + str(board[y][x]) + ' ', end='')
+                    print(' ' * (cell_width - 1) + str(board[y][x]) + ' ', end='')  # not empty cell
             print('|')
 
         print(' ' * left_width + '-' + ('-' * (cell_width + 1)) * dimension_x + '--')  # bottom border
@@ -80,13 +80,20 @@ class KnightTourPuzzle:
             print(f'{x + 1:>{cell_width}} ', end='')  # X coordinates
         print()
 
-    def start(self) -> None:
-        board = deepcopy(self.board)
-        y, x = self.starting_position[0], self.starting_position[1]
+    def main_cycle(self) -> None:
+        y, x = self.starting_position
 
         while True:
-            new_board, possible_moves = self.find_possible_moves(y, x, board)
-            self.print_board(new_board)
+            board = deepcopy(self.board)
+            board, possible_moves = self.get_board_and_moves(y, x, board)
+            if len(possible_moves) == 0:
+                if '_' in [i for row in board for i in row]:
+                    print('No more possible moves!\n'
+                          f'Your knight visited {self.visited_squares} squares!')
+                else:
+                    print('What a great tour! Congratulations!')
+                return
+            self.print_board(board)
 
             while True:
                 new_y, new_x = self.input_2_nums()
@@ -94,14 +101,15 @@ class KnightTourPuzzle:
                     break
                 print('Invalid move! ', end='')
 
-            board[y][x] = '*'
-            board[new_y][new_x] = 'X'
+            self.board[y][x] = '*'
+            self.board[new_y][new_x] = 'X'
+            self.visited_squares += 1
             y, x = new_y, new_x
 
 
 def main() -> None:
     puzzle = KnightTourPuzzle()
-    puzzle.start()
+    puzzle.main_cycle()
 
 
 if __name__ == '__main__':
