e7cf67dbf2a9c863ae7d4337bfa2ee973f14ac1c
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-03-21 00:15:52 +0700

    Add files via upload
    stage 3/5

--- manager.py
+++ manager.py
@@ -33,21 +33,56 @@ class FileManager:
             print('Invalid command')
         else:
             self.update_dir()
+            print(self.base_dir)
 
     def ls(self, param: str) -> None:
-        if not param:
-            pass
-        elif param == 'l':
-            pass
-        elif param == 'lh':
-            pass
+        if param in {'', '-l', '-lh'}:
+            directories, files = self.get_dirs_files()
+            self.print_directories(directories)
+            self.print_files(files, param)
         else:
             print('Invalid command')
+
+    @staticmethod
+    def get_dirs_files() -> tuple[list[str], dict[str: int]]:
+        directories: list[str] = []
+        files: dict[str: int] = {}
+        with os.scandir() as entries:
+            for entry in entries:
+                if entry.is_dir():
+                    directories.append(entry.name)
+                elif entry.is_file():
+                    files[entry.name] = entry.stat().st_size
+        return directories, files
+
+    @staticmethod
+    def print_directories(directories: list[str]):
+        for directory in directories:
+            print(directory)
+
+    @staticmethod
+    def print_files(files: dict[str: int], param: str):
+        for file in files:
+            print(file, end=' ')
+            if param == '':
+                print()
+            elif param == '-l':
+                print(files[file])
+            elif param == '-lh':
+                if files[file] < 1024:
+                    size = f'{files[file]}B'
+                elif files[file] < 1048576:
+                    size = f'{round(files[file] / 1024)}KB'
+                elif files[file] < 1073741824:
+                    size = f'{round(files[file] / 1048576)}MB'
+                else:
+                    size = f'{round(files[file] / 1073741824)}GB'
+                print(size)
 
     def start(self) -> None:
         print('Input the command')
         while True:
-            user_command = input()
+            user_command = self.choose()
             if user_command[0] == 'quit':
                 sys.exit()
             elif user_command[0] == 'pwd':
@@ -55,7 +90,10 @@ class FileManager:
             elif user_command[0] == 'cd':
                 self.cd(user_command[1])
             elif user_command[0] == 'ls':
-                self.ls(user_command[1])
+                if len(user_command) == 1:
+                    self.ls('')
+                else:
+                    self.ls(user_command[1])
             else:
                 print('Invalid command')
 
