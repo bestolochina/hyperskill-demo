fe9b9c82517056491553ae756ba3935b2ceb7754
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-03-27 01:54:14 +0700

    Add files via upload
    stage 4/6

--- easyrider.py
+++ easyrider.py
@@ -1,42 +1,27 @@
 import json
-import re
-
-
-def validate_stop_name(stop_name: str) -> bool:
-    suffixes = ['Road', 'Avenue', 'Boulevard', 'Street']
-    try:
-        name, suffix = stop_name.rsplit(maxsplit=1)
-    except ValueError:
-        return False
-    if suffix not in suffixes or not name[0].isupper():
-        return False
-    return True
-
-
-def validate_stop_type(stop_type: str) -> bool:
-    if stop_type not in ['', 'S', 'O', 'F']:
-        return False
-    return True
-
-
-def validate_a_time(a_time: str) -> bool:
-    if not re.match(r'^(2[0-3]|[01]\d):[0-5]\d$', a_time):
-        return False
-    return True
-
+import sys
 
 my_data: dict = json.loads(input())
-format_errors: dict = dict.fromkeys(['stop_name', 'stop_type', 'a_time'], 0)
+stops: dict[str, set[str]] = dict.fromkeys(['Start_stops', 'Transfer stops', 'Finish stops'])
+for stop_type in stops:
+    stops[stop_type] = set()
+# Start stops: 3 ['Bourbon Street', 'Pilotow Street', 'Prospekt Avenue']
+# Transfer stops: 3 ['Elm Street', 'Sesame Street', 'Sunset Boulevard']
+# Finish stops: 2 ['Sesame Street', 'Sunset Boulevard']
+
+lines: dict[str: dict[int, int]] = {'S': {}, 'F': {}}
 for stop_data in my_data:
-    if validate_stop_name(stop_data['stop_name']) is False:
-        format_errors['stop_name'] += 1
-    if validate_stop_type(stop_data['stop_type']) is False:
-        format_errors['stop_type'] += 1
-    if validate_a_time(stop_data['a_time']) is False:
-        format_errors['a_time'] += 1
+    if stop_data['stop_type'] not in {'S', 'F'}:
+        continue
+    if stop_data['bus_id'] not in lines[stop_data['stop_type']].keys():
+        lines[stop_data['stop_type']][stop_data['bus_id']] = 1
+    else:
+        print('There is no start or end stop for the line: 512.')
+        sys.exit()
+        # lines[stop_data['stop_type']][stop_data['bus_id']] += 1
+for stop_type in lines:
+    for line, num in lines[stop_type].items():
+        if num != 1:
+            print('There is no start or end stop for the line: 512.')
+            sys.exit()
 
-all_errors = sum(format_errors.values())
-
-print(f'Format validation: {all_errors} errors')
-for key, value in format_errors.items():
-    print(f'{key}: {value}')
