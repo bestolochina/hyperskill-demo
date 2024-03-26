9259f9c5514f1ccebec9ce82e73362f8fc21b360
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-03-26 13:43:54 +0700

    Add files via upload
    stage 3/6

--- easyrider.py
+++ easyrider.py
@@ -1,13 +1,42 @@
 import json
+import re
 
 
-my_data = json.loads(input())
-errors = dict.fromkeys(['bus_id', 'stop_id', 'stop_name', 'next_stop', 'stop_type', 'a_time'], 0)
-model = {'bus_id': int, 'stop_id': int, 'stop_name': str, 'next_stop': int, 'stop_type': str, 'a_time': str}
-my_data = json.loads(ddd)
-# print(json.dumps(my_data, indent=2))
+def validate_stop_name(stop_name: str) -> bool:
+    suffixes = ['Road', 'Avenue', 'Boulevard', 'Street']
+    try:
+        name, suffix = stop_name.rsplit(maxsplit=1)
+    except ValueError:
+        return False
+    if suffix not in suffixes or not name[0].isupper():
+        return False
+    return True
+
+
+def validate_stop_type(stop_type: str) -> bool:
+    if stop_type not in ['', 'S', 'O', 'F']:
+        return False
+    return True
+
+
+def validate_a_time(a_time: str) -> bool:
+    if not re.match(r'^(2[0-3]|[01]\d):[0-5]\d$', a_time):
+        return False
+    return True
+
+
+my_data: dict = json.loads(input())
+format_errors: dict = dict.fromkeys(['stop_name', 'stop_type', 'a_time'], 0)
 for stop_data in my_data:
-    for key, value in stop_data.items():
-        if (not isinstance(value, model[key])) or (not value):
-            errors[key] += 1
-print(errors)
+    if validate_stop_name(stop_data['stop_name']) is False:
+        format_errors['stop_name'] += 1
+    if validate_stop_type(stop_data['stop_type']) is False:
+        format_errors['stop_type'] += 1
+    if validate_a_time(stop_data['a_time']) is False:
+        format_errors['a_time'] += 1
+
+all_errors = sum(format_errors.values())
+
+print(f'Format validation: {all_errors} errors')
+for key, value in format_errors.items():
+    print(f'{key}: {value}')
