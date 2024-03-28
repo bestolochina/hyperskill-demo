81ba908fa4ff050407180bed82134b832962e716
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-03-28 11:27:31 +0700

    Add files via upload
    Easy Rider Bus Company - HyperSkill project

--- easyrider.py
+++ easyrider.py
@@ -1,20 +1,41 @@
 import json
-from collections import defaultdict
+import sys
 
 my_data: dict = json.loads(input())
-lines: dict[int: list[tuple[str, str]]] = defaultdict(list)
-print('Arrival time test:')
-for stop in my_data:
-    lines[stop['bus_id']].append((stop['stop_name'], stop['a_time']))
+stops: dict[int: dict[str: set[str]]] = {}
 
-ok = True
-for line, stops in lines.items():
-    prev_time = stops[0][1]
-    for i in range(1, len(stops)):
-        if stops[i][1] <= prev_time:
-            print(f'bus_id line {line}: wrong time on station {stops[i][0]}')
-            ok = False
-            break
-        prev_time = stops[i][1]
-if ok is True:
+# Find start and finish stops for each bus line
+for stop_data in my_data:
+    if stop_data['bus_id'] not in stops.keys():
+        stops[stop_data['bus_id']] = {'Start stops': set(),
+                                      'Transfer stops': set(),
+                                      'Finish stops': set(),
+                                      'All stops': set()}
+    stops[stop_data['bus_id']]['All stops'].add(stop_data['stop_name'])
+    if stop_data['stop_type'] == 'S':
+        stops[stop_data['bus_id']]['Start stops'].add(stop_data['stop_name'])
+    if stop_data['stop_type'] == 'F':
+        stops[stop_data['bus_id']]['Finish stops'].add(stop_data['stop_name'])
+
+# Find transfer stops
+for my_bus_id, my_bus_stops in stops.items():
+    for bus_id, bus_stops in stops.items():
+        if bus_id == my_bus_id:
+            continue
+        my_bus_stops['Transfer stops'].update(my_bus_stops['All stops'] & bus_stops['All stops'])
+
+# Aggregating all start, finish and transfer stops
+stf_stops = set()
+for bus_stops in stops.values():
+    stf_stops.update(bus_stops['Start stops'] | bus_stops['Finish stops'] | bus_stops['Transfer stops'])
+
+wrong_stops = set()
+for stop_data in my_data:
+    if stop_data['stop_type'] == 'O' and stop_data['stop_name'] in stf_stops:
+        wrong_stops.add(stop_data['stop_name'])
+
+print('On demand stops test:')
+if len(wrong_stops) == 0:
     print('OK')
+else:
+    print(f'Wrong stop type: {sorted(list(wrong_stops))}')
