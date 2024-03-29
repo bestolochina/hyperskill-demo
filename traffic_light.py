eaf3ca1a7d5e83f8f9eb91097b7001e97b80c095
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-03-29 21:10:42 +0700

    Add files via upload
    stage 5/6

--- traffic_light.py
+++ traffic_light.py
@@ -1,11 +1,17 @@
+import time
 import os
 import sys
+import threading
 
 
 class TrafficLight:
     def __init__(self):
+        self.state: str = 'Not Started'
+        self.thread_system: threading.Thread = threading.Thread(target=self.queue_thread, name='QueueThread')
         self.roads_num: int = 0
         self.interval: int = 0
+        self.time_0: float = 0
+        self.timer: int = 0
 
     @staticmethod
     def input_int(prompt: str = '', err: str = 'Error! Incorrect Input. Try again: ') -> int:
@@ -48,16 +54,39 @@ class TrafficLight:
     def delete_road() -> None:
         print('Road deleted')
 
-    @staticmethod
-    def open_system() -> None:
-        print('System opened')
+    def open_system(self) -> None:
+        self.timer = round(time.time() - self.time_0)
+        self.print_system()
+        self.state = 'System'
 
-    def main(self) -> None:
+    def queue_thread(self) -> None:
+        while self.state != 'Terminate':
+            if self.state == 'System':
+                this_time = round(time.time() - self.time_0)
+                if this_time == self.timer:
+                    continue
+                self.timer = this_time
+                self.print_system()
+
+    def print_system(self) -> None:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f'! {self.timer}s. have passed since system startup !')
+        print(f'! Number of roads: {self.roads_num} !')
+        print(f'! Interval: {self.interval} !')
+        print('! Press "Enter" to open menu !')
+
+    def start(self) -> None:
         print('Welcome to the traffic management system!')
-        self.roads_num: int = self.input_int(prompt='Input the number of roads: ')
-        self.interval: int = self.input_int(prompt='Input the interval: ')
+        self.roads_num = self.input_int(prompt='Input the number of roads: ')
+        self.interval = self.input_int(prompt='Input the interval: ')
+        self.time_0 = time.time()
         self.clear_screen(0)
-        while True:
+        self.state = 'Menu'
+        self.thread_system.start()
+        self.menu()
+
+    def menu(self) -> None:
+        while self.state == 'Menu':
             choice = self.choose()
             if choice == '1':
                 self.add_road()
@@ -66,10 +95,13 @@ class TrafficLight:
             elif choice == '3':
                 self.open_system()
             elif choice == '0':
+                self.state = 'Terminate'
+                print('Bye!')
                 sys.exit()
             self.clear_screen()
+            self.state = 'Menu'
 
 
 if __name__ == '__main__':
     my_system = TrafficLight()
-    my_system.main()
+    my_system.start()
