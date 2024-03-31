8020f1a80a5663a390f7b8e22ebb1bd921c6ead0
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-04-01 01:52:54 +0700

    Add files via upload
    Traffic Light - HyperSkill project

--- traffic_light.py
+++ traffic_light.py
@@ -22,23 +22,34 @@ class CircularQueue:
             raise IndexError("Queue is full")
 
         if len(self.queue) == 0:  # The queue is empty?
-            self.queue.append(Road(name, True, interval))
+            self.queue.append(Road(name, True, interval + 1))
             return
 
         # Add a new road with the correct countdown
-        if self.queue[-1].status is True:
-            countdown = self.queue[-1].countdown
-        else:
-            countdown = self.queue[-1].countdown + interval
-        self.queue.append(Road(name, False, countdown))
+        open_road_index = self.open_road_index()
 
-        # Normalize the queue countdowns after adding a new road
-        min_countdown_road = self.min_countdown_road()
-        for road in self.queue:
-            if road == min_countdown_road:
-                break
-            countdown += interval
-            road.countdown = countdown
+        if self.queue[-1].status is True:  # The last road is open?
+            next_countdown = self.queue[-1].countdown
+            self.queue.append(Road(name, False, next_countdown))
+            for i in range(len(self.queue)-2):
+                next_countdown += interval
+                self.queue[i].countdown = next_countdown
+
+        else:  # The last road is closed?
+            next_countdown = self.queue[-1].countdown + interval
+            self.queue.append(Road(name, False, next_countdown))
+
+            if open_road_index is not None:  # Is there an open road?
+                for i in range(0, open_road_index):
+                    next_countdown += interval
+                    self.queue[i].countdown = next_countdown
+            else:  # There is no open road
+                min_countdown = min(_.countdown for _ in self.queue)
+                for road in self.queue:
+                    if road.countdown == min_countdown:
+                        break
+                    next_countdown += interval
+                    road.countdown = next_countdown
 
     def dequeue(self, interval: int) -> Road:
         if len(self.queue) == 0:
@@ -52,41 +63,63 @@ class CircularQueue:
         if return_road.status is False:  # The deleted road is closed?
 
             # Normalize the queue countdowns after deleting a closed road
-            min_countdown_road = self.min_countdown_road()
-            countdown = self.queue[-1].countdown
+            min_countdown = min(_.countdown for _ in self.queue)
+            next_countdown = return_road.countdown
             for road in self.queue:
-                if road == min_countdown_road:
+                if road.countdown == min_countdown:
                     break
-                countdown += interval
-                road.countdown = countdown
+                road.countdown = next_countdown
+                next_countdown += interval
         else:  # The deleted road is open?
-            pass  # No need to normalize
+            pass  # No need to normalize!
 
         return return_road
 
-    def is_empty(self) -> bool:
-        return len(self.queue) == 0
-
-    def is_full(self) -> bool:
-        return len(self.queue) == self.queue.maxlen
-
-    def size(self) -> int:
-        return len(self.queue)
-
-    def output(self) -> list[Road]:
-        return list(self.queue)
-
-    def min_countdown_road(self) -> Road:
+    def time_shift(self, interval):
         if len(self.queue) == 0:
-            raise IndexError("Queue is empty")
-        return min(self.queue, key=lambda x: x.countdown)
+            return
+
+        for road in self.queue:
+            road.countdown -= 1
+
+        if len(self.queue) == 1 and self.queue[0].countdown == 0:
+            self.queue[0].status = True
+            self.queue[0].countdown = interval
+            return
+
+        max_countdown = max(_.countdown for _ in self.queue)
+        for road in self.queue:
+            if road.countdown == 0:
+                if road.status is False:  # Closed road?
+                    road.status = True
+                    road.countdown = interval
+                else:  # Open road?
+                    road.status = False
+                    road.countdown = max_countdown + interval
+
+    def output(self, roads_num: int, interval: int, timer: int) -> None:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f'! {timer}s. have passed since system startup !')
+        print(f'! Number of roads: {roads_num} !')
+        print(f'! Interval: {interval} !')
+        for road in self.queue:
+            status = 'open' if road.status is True else 'closed'
+            print(f'Road "{road.name}" will be {status} for {road.countdown}s.')
+        print('! Press "Enter" to open menu !')
+
+    def open_road_index(self) -> int | None:
+        open_list = []
+        for i in range(len(self.queue)):
+            if self.queue[i].status is True:
+                open_list.append(i)
+        assert len(open_list) <= 1
+        return open_list[0] if open_list else None
 
 
 class TrafficLight:
     def __init__(self):
         self.state: str = 'Not Started'
         self.thread_system: Thread = Thread(target=self.queue_thread, name='QueueThread')
-        # self.thread_norm: Thread = Thread(target=self.queue_norm)
         self.roads_num: int = self.input_int(prompt='Input the number of roads: ')
         self.roads: CircularQueue = CircularQueue(self.roads_num)
         self.interval: int = self.input_int(prompt='Input the interval: ')
@@ -150,35 +183,20 @@ class TrafficLight:
             self.state = 'Menu'
 
     def open_system(self) -> None:
-        self.timer = round(time.time() - self.time_0)
-        self.print_system()
+        # self.timer = round(time.time() - self.time_0)
+        # self.roads.output(self.roads_num, self.interval, self.timer)
         self.state = 'System'
 
     def queue_thread(self) -> None:
         while self.state != 'Terminate':
             if self.state == 'Norm':
                 continue
-            this_time = round(time.time() - self.time_0)
-            if this_time == self.timer:
+            if time.time() - self.time_0 < self.timer + 1:
                 continue
-            self.timer = this_time
-            self.time_shift()
+            self.timer += 1
+            self.roads.time_shift(self.interval)
             if self.state == 'System':
-                self.print_system()
-
-    def time_shift(self):
-        pass
-
-    def print_system(self) -> None:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print(f'! {self.timer}s. have passed since system startup !')
-        print(f'! Number of roads: {self.roads_num} !')
-        print(f'! Interval: {self.interval} !')
-        print()
-        for road in self.roads.output():
-            print(road)
-        print()
-        print('! Press "Enter" to open menu !')
+                self.roads.output(self.roads_num, self.interval, self.timer)
 
     def start(self) -> None:
         self.clear_screen(0)
