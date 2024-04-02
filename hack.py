66903f262ddd1f7a85ce760863f9628bbd26db3e
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-04-03 02:06:42 +0700

    Add files via upload
    stage 4/5

--- hack.py
+++ hack.py
@@ -1,31 +1,48 @@
 import socket
 import sys
+import json
 from itertools import product
 
 
-assert len(sys.argv) == 3
-hostname = sys.argv[1]
-port = int(sys.argv[2])
-address = (hostname, port)
-symbols = 'abcdefghijklmnopqrstuvwxyz0123456789'
-password = ''
+class PasswordHacker:
+    def __init__(self, hostname: str, port: int):
+        self.hostname: str = hostname
+        self.port: int = port
+        self.address: tuple[str, int] = (self.hostname, self.port)
+        self.login_file: str = 'logins.txt'
+        self.login: str = ''
+        self.password: str = ''
 
-with socket.socket() as my_socket:
-    my_socket.connect(address)
+    def start(self):
+        self.find_login()
+        self.find_password()
+        self.print_output()
 
-    repeat = 1
-    while not password:
-        for passw in product(symbols, repeat=repeat):
-            my_socket.send(''.join(passw).encode())
-            response = my_socket.recv(1024).decode()
+    def find_login(self):
+        with socket.socket() as my_socket, open(self.login_file) as file:
+            password = '1'
+            my_socket.connect(self.address)
+            for line in file:
+                line = line.strip()
+                for login in map(lambda x: ''.join(x), product(*([letter.lower(), letter.upper()] for letter in line))):
+                    my_socket.send(json.dumps({'login': login, 'password': password}).encode())
+                    response = json.loads(my_socket.recv(1024).decode())
+                    if response['result'] == 'Wrong password!':
+                        self.login = login
+                        return
 
-            if response == 'Wrong password!':
-                continue
-            elif response == 'Connection success!':
-                password = ''.join(passw)
-                break
-            elif response == 'Too many attempts':
-                sys.exit('--- Game over ---')
-        repeat += 1
+    def find_password(self):
+        pass
 
-print(password)
+    def print_output(self):
+        raise NotImplementedError
+
+
+def main() -> None:
+    assert len(sys.argv) == 3
+    hacker = PasswordHacker(hostname=sys.argv[1], port=int(sys.argv[2]))
+    hacker.start()
+
+
+if __name__ == '__main__':
+    main()
