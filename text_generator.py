3ce518488b5813f7487c0ac3766ca466235227af
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-04-09 15:30:02 +0700

    Add files via upload
    stage 5/6

--- text_generator.py
+++ text_generator.py
@@ -4,6 +4,7 @@ import sys
 from nltk.tokenize import WhitespaceTokenizer
 from nltk import ngrams
 from collections import defaultdict, Counter
+from random import choice, choices
 
 
 class TextGenerator:
@@ -20,6 +21,12 @@ class TextGenerator:
         self.logger.debug(f'Corpus - {self.corpus[:50]} ...')
         self.tokens: list[str] = []
         self.bigrams_dict: dict[str, dict[str, int]] = {}
+        self.sentences: list[list[str]] = []
+
+    def get_corpus(self) -> str:
+        with open(self.file_name, "r", encoding="utf-8") as file:
+            text = file.read()
+        return text
 
     @staticmethod
     def get_file_name() -> str:
@@ -32,7 +39,6 @@ class TextGenerator:
         self.logger.debug(f'Tokens - {self.tokens[:8]} ...')
 
     def get_bigrams(self) -> None:
-
         bigrams = ngrams(self.tokens, 2)
         bigrams_counter = Counter(bigrams)
 
@@ -41,34 +47,43 @@ class TextGenerator:
         for bigram, num in bigrams_counter.items():
             self.bigrams_dict[bigram[0]][bigram[1]] = num
 
-        log_message: str = 'Corpus - '
-        keys: list[str] = list(self.bigrams_dict.keys())[:5]
+        log_message: str = 'Bigrams - '
+        keys: list[str] = list(self.bigrams_dict.keys())[:2]
         for key in keys:
             log_message += f'{self.bigrams_dict[key]}'
-        self.logger.debug(f'Corpus - {log_message} ...')
+        self.logger.debug(f'{log_message} ...')
 
-    def print_bigram(self) -> None:
-        while True:
-            try:
-                head = input().strip()
-                if head.lower() == 'exit':
-                    sys.exit()
-                print(f'Head: {head}')
-                for tail, count in sorted(self.bigrams_dict[head].items(), key=lambda x: x[1], reverse=True):
-                    print(f'Tail: {tail} Count: {count}')
-                print()
-            except KeyError:
-                print('Key Error. The requested word is not in the model. Please input another word.\n')
+    def make_sentences(self) -> None:
+        first_word: str = choice(list(self.bigrams_dict.keys()))
+        for i in range(10):
+            self.logger.debug(f'First word - {first_word}')
+            sentence: list[str] = self.make_sentence(first_word)
+            self.logger.debug(f'Sentence - {sentence}')
+            self.sentences.append(sentence)
+            first_word = sentence[-1]
 
-    def get_corpus(self) -> str:
-        with open(self.file_name, "r", encoding="utf-8") as file:
-            text = file.read()
-        return text
+    def make_sentence(self, first_word: str) -> list[str]:
+        sentence: list[str] = [first_word]
+        for i in range(9):
+            word: str = sentence[-1]
+            population: list[str] = list(self.bigrams_dict[word].keys())
+            self.logger.debug(f'Population - {population}')
+            weights: list[int] = list(self.bigrams_dict[word].values())
+            self.logger.debug(f'Weights - {weights}')
+            next_word: str = choices(population=population, weights=weights)[0]
+            self.logger.debug(f'Next word - {next_word}')
+            sentence.append(next_word)
+        return sentence
+
+    def print_sentences(self) -> None:
+        for sentence in self.sentences:
+            print(*sentence)
 
     def start(self) -> None:
         self.tokenize()
         self.get_bigrams()
-        self.print_bigram()
+        self.make_sentences()
+        self.print_sentences()
 
 
 def main() -> None:
