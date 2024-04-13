12510dc16c682fbd117578e4245ef1bf5be04377
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-04-13 21:14:36 +0700

    Add files via upload
    stage 4/5

--- code_analyzer.py
+++ code_analyzer.py
@@ -1,8 +1,12 @@
+import sys
+import os.path
 import re
 
 
 class StaticCodeAnalyzer:
-    def __init__(self) -> None:
+    def __init__(self, path: str) -> None:
+        self.path: str = path
+        self.files: list[str] = []
         self.file: str = ''
         self.code: list[str] = []
         self.errors: list[dict[str, str | callable]] = \
@@ -17,12 +21,30 @@ class StaticCodeAnalyzer:
         self.char: str = ''
         self.char_index: int = 0
 
-    def read_file(self) -> None:
-        self.file = input()
-        with open(self.file) as file:
-            self.code = []
-            for line in file:
-                self.code.append(line.rstrip('\n'))
+    def read_files(self) -> None:
+        if os.path.isfile(self.path):
+            self.files = [self.path]
+        else:
+            self.files = []
+            for (root, dirs, files) in os.walk(self.path):
+                for name in files:
+                    path = os.path.join(root, name)
+                    if path.endswith('.py'):
+                        self.files.append(path)
+
+    def close_quote(self) -> bool:
+        while True:
+            try:
+                next_quot_index = self.line.find(self.char, self.char_index + 1)
+            except IndexError:
+                return False
+            if next_quot_index == -1:
+                return False
+            if self.line[next_quot_index - 1] == '\\':
+                self.char_index = next_quot_index
+                continue
+            self.char_index = next_quot_index
+            return True
 
     def s001(self) -> bool:  # line length
         return len(self.line) > 79
@@ -45,20 +67,6 @@ class StaticCodeAnalyzer:
             elif self.char == ';':
                 return True
             self.char_index += 1
-
-    def close_quote(self):
-        while True:
-            try:
-                next_quot_index = self.line.find(self.char, self.char_index + 1)
-            except IndexError:
-                return False
-            if next_quot_index == -1:
-                return False
-            if self.line[next_quot_index - 1] == '\\':
-                self.char_index = next_quot_index
-                continue
-            self.char_index = next_quot_index
-            return True
 
     def s004(self) -> bool:  # two spaces before inline comments
         self.char_index = 0
@@ -93,29 +101,36 @@ class StaticCodeAnalyzer:
             self.char_index += 1
 
     def s006(self) -> bool:  # More than two blank lines preceding a code line
-        if not self.line.strip():
-            return False
-        elif self.line_number < 3:
+        if not self.line.strip() or self.line_number < 3:
             return False
         elif not ''.join(self.code[self.line_number - 3:self.line_number]):
             return True
         return False
 
     def check(self) -> None:
-        for self.line_number in range(len(self.code)):
-            self.line = self.code[self.line_number]
+        for self.line_number, self.line in enumerate(self.code):
             for error in self.errors:
                 if error['function']():
-                    print(f'Line {self.line_number + 1}: {error['code']} {error['message']}')
+                    print(f'{self.file}: Line {self.line_number + 1}: {error['code']} {error['message']}')
 
     def start(self) -> None:
-        self.read_file()
-        self.check()
+        self.read_files()  # get the list of files
+        for self.file in self.files:
+            with open(self.file) as file:
+                self.code = []
+                for line in file:
+                    self.code.append(line.rstrip('\n'))
+            self.check()
 
 
 def main() -> None:
-    analyzer = StaticCodeAnalyzer()
-    analyzer.start()
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
+    if os.path.exists(path=path):
+        analyzer = StaticCodeAnalyzer(path=path)
+        analyzer.start()
+    else:
+        print(f"{path} doesn't exist.")
 
 
 if __name__ == '__main__':
