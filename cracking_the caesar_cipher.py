725863bc347c9495b56101edec1c3866e1c92808
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-04-18 01:47:01 +0700

    Add files via upload
    Cracking the Caesar cipher - HyperSkill project

--- cracking_the caesar_cipher.py
+++ cracking_the caesar_cipher.py
@@ -1,49 +1,51 @@
 import string
-import sys
 
 
-class Cipher:
-    def __init__(self) -> None:
-        self.key_sentence: list[str] = []
-        self.key: str = 'butterscotch'
-        self.shift: int = 0
+class Vigenere:
+    def __init__(self, key: str = '') -> None:
+        self.key: str = key
         self.alphabet = string.ascii_lowercase
-        self.message: list[str] = []
-        self.text: str = ''
 
-    def decipher(self) -> None:
-        self.text = ''
-        for char in self.message:
-            letter: str = self.alphabet[(self.alphabet.index(char) - self.shift) % len(self.alphabet)]
-            if letter == 'x':
-                letter = ' '
-            self.text += letter
-
-    def get_shift(self) -> None:
-        for i in range(len(self.key_sentence)):
-            shift: int = (self.alphabet.index(self.key_sentence[i])
-                          - self.alphabet.index(self.key[0])) % len(self.alphabet)
-            for j in range(1, len(self.key)):
-                if shift != (self.alphabet.index(self.key_sentence[i + j])
-                             - self.alphabet.index(self.key[j])) % len(self.alphabet):
-                    break
+    def encode(self, text: str | list[str]) -> str:
+        message: str = ''
+        for i in range(len(text)):
+            if text[i] == ' ':
+                char: str = ' '
             else:
-                self.shift = shift
-                return
-        print(f'There is no {self.key} in the first sentence')
-        sys.exit()
+                new_pos: int = ((self.alphabet.index(text[i]) + self.alphabet.index(self.key[i % len(self.key)]))
+                                % len(self.alphabet))
+                char = self.alphabet[new_pos]
+            message += char
+        return message
 
-    def start(self) -> None:
-        self.key_sentence = input().split()
-        self.message = input().split()
-        self.get_shift()
-        self.decipher()
-        print(self.text)
+    def decode(self, message: str | list[str]) -> str:
+        text: str = ''
+        for i in range(len(message)):
+            old_pos: int = ((self.alphabet.index(message[i]) - self.alphabet.index(self.key[i % len(self.key)]))
+                            % len(self.alphabet))
+            char: str = self.alphabet[old_pos]
+            if char == 'x':
+                char = ' '
+            text += char
+        return text
+
+    def find_key(self, key_length: int, plain_text: str | list[str], encoded_text: str | list[str]) -> None:
+        self.key = ''
+        for i in range(key_length):
+            shift: int = ((self.alphabet.index(encoded_text[i]) - self.alphabet.index(plain_text[i]))
+                          % len(self.alphabet))
+            char: str = self.alphabet[shift]
+            self.key += char
 
 
 def main() -> None:
-    my_cypher = Cipher()
-    my_cypher.start()
+    my_cypher = Vigenere()
+    key_length = int(input())
+    plain_text = input().split()
+    encoded_text = input().split()
+    message = input().split()
+    my_cypher.find_key(key_length, plain_text, encoded_text)
+    print(my_cypher.decode(message))
 
 
 if __name__ == '__main__':
