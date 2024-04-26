120e71862a3907691050008acf4e39a21c016399
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-04-27 00:19:54 +0700

    Add files via upload
    stage 4/6

--- sorting_tool.py
+++ sorting_tool.py
@@ -3,19 +3,21 @@ from math import floor
 
 
 class SortingTool:
-    def __init__(self, data_type) -> None:
-        self.data_type = data_type
-        self.data: list = []
+    def __init__(self, sort_integers: bool = False, data_type: str = 'long') -> None:
+        self.sort_integers: bool = sort_integers
+        self.data_type: str = data_type
+        self.data: list[int | str] = []
         self.prompts: dict[str, str] = {
             'long': 'Total numbers: {0}.\nThe greatest number: {1} ({2} time(s), {3}%).',
             'line': 'Total lines: {0}.\nThe longest line:\n{1}\n({2} time(s), {3}%).',
-            'word': 'Total words: {0}.\nThe longest word: {1} ({2} time(s), {3}%).'}
+            'word': 'Total words: {0}.\nThe longest word: {1} ({2} time(s), {3}%).',
+            'sorted_integers': 'Total numbers: {0}.\nSorted data: {sorted_integers}'}
 
     def user_input(self) -> None:
         self.data = []
         while True:
             try:
-                data = input()
+                data: str = input()
                 self.data.append(data)
             except EOFError:
                 break
@@ -30,11 +32,15 @@ class SortingTool:
         return len(element)
 
     def process_data(self) -> None:
-        length = len(self.data)
-        greatest = max(self.data, key=self.key)
-        num_greatest = self.data.count(greatest)
-        percent_greatest = floor(100 * (num_greatest / length))
-        print(self.prompts[self.data_type].format(length, greatest, num_greatest, percent_greatest))
+        length: int = len(self.data)
+        greatest: int | str = max(self.data, key=self.key)
+        num_greatest: int = self.data.count(greatest)
+        percent_greatest: int = floor(100 * (num_greatest / length))
+        if self.sort_integers:
+            sort_int: str = ' '.join([str(_) for _ in sorted(self.data)])
+            print(self.prompts['sorted_integers'].format(length, sorted_integers=sort_int))
+        else:
+            print(self.prompts[self.data_type].format(length, greatest, num_greatest, percent_greatest))
 
     def start(self) -> None:
         self.user_input()
@@ -44,8 +50,12 @@ class SortingTool:
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('-dataType', dest='data_type', choices=['long', 'line', 'word'], default='word')
+    parser.add_argument('-sortIntegers', dest='sort_integers', action='store_true')
     args = parser.parse_args()
-    sorting_tool = SortingTool(args.data_type)
+    if args.sort_integers:
+        sorting_tool = SortingTool(sort_integers=args.sort_integers)
+    else:
+        sorting_tool = SortingTool(data_type=args.data_type)
     sorting_tool.start()
 
 
