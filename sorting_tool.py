9233df6d26792ec016f00e089ff5a6bcb3dee87f
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-04-27 22:54:44 +0700

    Add files via upload
    stage 5/6

--- sorting_tool.py
+++ sorting_tool.py
@@ -3,15 +3,18 @@ from math import floor
 
 
 class SortingTool:
-    def __init__(self, sort_integers: bool = False, data_type: str = 'long') -> None:
-        self.sort_integers: bool = sort_integers
+    def __init__(self, data_type: str = 'long', sort_type: str = 'natural') -> None:
+        self.sort_type: str = sort_type
         self.data_type: str = data_type
         self.data: list[int | str] = []
-        self.prompts: dict[str, str] = {
-            'long': 'Total numbers: {0}.\nThe greatest number: {1} ({2} time(s), {3}%).',
-            'line': 'Total lines: {0}.\nThe longest line:\n{1}\n({2} time(s), {3}%).',
-            'word': 'Total words: {0}.\nThe longest word: {1} ({2} time(s), {3}%).',
-            'sorted_integers': 'Total numbers: {0}.\nSorted data: {sorted_integers}'}
+        self.sorted_data: list[int | str] = []
+        self.prompts: dict[tuple[str, str], dict[str, str]] = {
+            ('long', 'natural'): {'prompt': 'Total numbers: {total}.\nSorted data: {sorted_data}', 'separator': ' '},
+            ('long', 'byCount'): {'prompt': 'Total numbers: {total}.\n{sorted_data}', 'separator': '\n'},
+            ('word', 'natural'): {'prompt': 'Total words: {total}.\nSorted data: {sorted_data}', 'separator': ' '},
+            ('word', 'byCount'): {'prompt': 'Total words: {total}.\n{sorted_data}', 'separator': '\n'},
+            ('line', 'natural'): {'prompt': 'Total lines: {total}.\nSorted data:\n{sorted_data}', 'separator': '\n'},
+            ('line', 'byCount'): {'prompt': 'Total lines: {total}.\n{sorted_data}', 'separator': '\n'}}
 
     def user_input(self) -> None:
         self.data = []
@@ -26,21 +29,31 @@ class SortingTool:
         elif self.data_type == 'long':
             self.data = [int(_) for _ in ' '.join(self.data).split()]
 
-    def key(self, element: str | int) -> str | int:
-        if self.data_type == 'long':
-            return element
-        return len(element)
+    def key(self, element: str | int) -> str | int | tuple[int | str, int]:
+        match self.data_type, self.sort_type:
+            case 'long', 'natural':
+                return element
+            case _, 'byCount':
+                return self.data.count(element), element
+            case _, 'natural':
+                return element
+
+    def sort_data(self) -> str:
+        separator: str = self.prompts[self.data_type, self.sort_type]['separator']
+        if self.sort_type == 'natural':
+            data = sorted(self.data, key=self.key)
+            return separator.join([str(_) for _ in data])
+        else:
+            data = sorted(set(self.data), key=self.key)
+            output_list = \
+                [str(x) + ': ' + str((n := self.data.count(x))) + ' time(s), '  # '4: 1 time(s), 14%'
+                 + str(int(100*n / len(self.data))) + '%' for x in data]
+            return separator.join(output_list)
 
     def process_data(self) -> None:
-        length: int = len(self.data)
-        greatest: int | str = max(self.data, key=self.key)
-        num_greatest: int = self.data.count(greatest)
-        percent_greatest: int = floor(100 * (num_greatest / length))
-        if self.sort_integers:
-            sort_int: str = ' '.join([str(_) for _ in sorted(self.data)])
-            print(self.prompts['sorted_integers'].format(length, sorted_integers=sort_int))
-        else:
-            print(self.prompts[self.data_type].format(length, greatest, num_greatest, percent_greatest))
+        total: int = len(self.data)
+        data = self.sort_data()
+        print(self.prompts[self.data_type, self.sort_type]['prompt'].format(total=total, sorted_data=data))
 
     def start(self) -> None:
         self.user_input()
@@ -50,14 +63,15 @@ class SortingTool:
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('-dataType', dest='data_type', choices=['long', 'line', 'word'], default='word')
-    parser.add_argument('-sortIntegers', dest='sort_integers', action='store_true')
+    parser.add_argument('-sortingType', dest='sort_type', choices=['natural', 'byCount'], default='natural')
     args = parser.parse_args()
-    if args.sort_integers:
-        sorting_tool = SortingTool(sort_integers=args.sort_integers)
-    else:
-        sorting_tool = SortingTool(data_type=args.data_type)
+    sorting_tool = SortingTool(data_type=args.data_type, sort_type= args.sort_type)
     sorting_tool.start()
 
 
 if __name__ == '__main__':
     main()
+
+# 1 -2   333 4
+# 42
+# 1                 1
