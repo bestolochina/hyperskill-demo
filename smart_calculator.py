3d23cc98793e7dfeeaea33980331febd18de0ebd
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-04-30 02:42:59 +0700

    Add files via upload
    stage 6/7

--- smart_calculator.py
+++ smart_calculator.py
@@ -8,55 +8,61 @@ class SmartCalculator:
 
     def main_loop(self) -> None:
         while True:
+            self.result = 0
             user_input = input().strip()
-            if user_input == '/exit':
-                print('Bye!')
-                sys.exit()
-            elif user_input == '/help':
-                print('The program adds and subtracts numbers')
-            elif not user_input:
+            if not user_input:
                 continue
+            elif user_input[0] == '/':
+                if user_input == '/exit':
+                    print('Bye!')
+                    sys.exit()
+                elif user_input == '/help':
+                    print('The program adds and subtracts numbers')
+                else:
+                    print('Unknown command')
             else:
-                self.calculate_expression(user_input)
-                self.print_result()
+                if self.calculate_expression(user_input) == 'err':
+                    print('Invalid expression')
+                else:
+                    print(self.result)
 
-    def calculate_expression(self, user_input: str) -> None:
+    def calculate_expression(self, user_input: str) -> str | None:
         self.data = [_ for _ in user_input.split()]
 
-        self.result += self.get_int(self.data[0])  # First number
+        if (first := self.get_int(self.data[0])) is None:
+            return 'err'
+        self.result += first  # First number
 
         i: int = 1
         while i < len(self.data):
-            factor = self.get_sign(self.data[i])
+            if (factor := self.get_sign(self.data[i])) is None:
+                return 'err'
             i += 1
 
             if i >= len(self.data):
                 return
 
-            num = self.get_int(self.data[i])
+            if (num := self.get_int(self.data[i])) is None:
+                return 'err'
 
             self.result += factor * num
             i += 1
 
     @staticmethod
-    def get_int(string: str) -> int:
+    def get_int(string: str) -> int | None:
         try:
             return int(string)
         except ValueError:
-            sys.exit(f'{string} is not an integer')
+            return None
 
     @staticmethod
-    def get_sign(string: str) -> int:
+    def get_sign(string: str) -> int | None:
         if not set(string).issubset({'-', '+'}):
-            sys.exit(f'{string} is not a sign')
+            return None
         if string.count('-') % 2:
             return -1
         else:
             return 1
-
-    def print_result(self) -> None:
-        print(self.result)
-        self.result = 0
 
 
 def main() -> None:
