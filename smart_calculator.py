a509018c523084d64c8a4b9e425cdee7891cf34f
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-04-30 04:23:53 +0700

    Add files via upload
    stage 7/7

--- smart_calculator.py
+++ smart_calculator.py
@@ -1,14 +1,14 @@
 import sys
+from string import ascii_letters
 
 
 class SmartCalculator:
     def __init__(self) -> None:
+        self.vars: dict[str, int] = {}
         self.data: list[str] = []
-        self.result: int = 0
 
     def main_loop(self) -> None:
         while True:
-            self.result = 0
             user_input = input().strip()
             if not user_input:
                 continue
@@ -20,45 +20,57 @@ class SmartCalculator:
                     print('The program adds and subtracts numbers')
                 else:
                     print('Unknown command')
-            else:
-                if self.calculate_expression(user_input) == 'err':
-                    print('Invalid expression')
-                else:
-                    print(self.result)
+            elif '=' in user_input:  # Assignment
+                if isinstance((result := self.assign_value(user_input)), str):
+                    print(result)
+            else:  # Expression
+                print(self.calculate_expression(user_input))
 
-    def calculate_expression(self, user_input: str) -> str | None:
+    def calculate_expression(self, user_input: str) -> str | int:
         self.data = [_ for _ in user_input.split()]
 
-        if (first := self.get_int(self.data[0])) is None:
-            return 'err'
-        self.result += first  # First number
+        if isinstance((result := self.get_int(self.data[0])), str):  # The first number
+            return result
 
         i: int = 1
         while i < len(self.data):
-            if (factor := self.get_sign(self.data[i])) is None:
-                return 'err'
+            if isinstance((factor := self.get_sign(self.data[i])), str):
+                return factor
             i += 1
 
             if i >= len(self.data):
-                return
+                return result
 
-            if (num := self.get_int(self.data[i])) is None:
-                return 'err'
+            if isinstance((num := self.get_int(self.data[i])), str):
+                return num
 
-            self.result += factor * num
+            result += factor * num
             i += 1
+        return result
 
-    @staticmethod
-    def get_int(string: str) -> int | None:
+    def assign_value(self, user_input: str) -> str | None:
+        var, expression = [_.strip() for _ in user_input.split(sep='=', maxsplit=1)]
+        if not set(var).issubset(ascii_letters):
+            return 'Invalid identifier'
+        if isinstance((result := self.calculate_expression(expression)), str):
+            return result
+        self.vars[var] = result
+
+    def get_int(self, string: str) -> int | str:
         try:
             return int(string)
         except ValueError:
-            return None
+            if not set(string).issubset(ascii_letters):
+                return 'Invalid identifier'
+        try:
+            return self.vars[string]
+        except KeyError:
+            return 'Unknown variable'
 
     @staticmethod
-    def get_sign(string: str) -> int | None:
+    def get_sign(string: str) -> int | str:
         if not set(string).issubset({'-', '+'}):
-            return None
+            return 'Invalid expression'
         if string.count('-') % 2:
             return -1
         else:
