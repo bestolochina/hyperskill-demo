94925908e42d332362cfc7b940189fb0904c7856
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-05-03 23:19:45 +0700

    Add files via upload
    stage 4/6

--- numeric_matrix_processor.py
+++ numeric_matrix_processor.py
@@ -2,6 +2,12 @@ import sys
 import numpy as np
 
 
+class ShapeException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
 class MyArray(np.ndarray):
     def __new__(cls, input_array) -> np.ndarray:
         obj = np.asarray(input_array).view(cls)
@@ -15,19 +21,61 @@ class MyArray(np.ndarray):
 
 class NumericMatrixProcessor:
     def __init__(self) -> None:
-        self.n_rows: int = 0
-        self.n_columns: int = 0
+        self.choices: dict[str, callable] = {'1': self.mat_plus_mat,
+                                             '2': self.mat_times_num,
+                                             '3': self.mat_times_mat,
+                                             '0': self.exit}
 
-    def matrix_input(self) -> np.ndarray:
+    def main_menu(self) -> None:
+        while True:
+            print('1. Add matrices\n2. Multiply matrix by a constant\n3. Multiply matrices\n0. Exit')
+            choice = input('Your choice: ')
+            if choice in self.choices.keys():
+                try:
+                    result = self.choices[choice]()
+                except ShapeException as err:
+                    print(err.message)
+                else:
+                    print(f'The result is:\n{result}\n')
+
+    def mat_plus_mat(self):
+        mat1 = self.matrix_input('Enter size of first matrix: ', 'Enter first matrix:')
+        mat2 = self.matrix_input('Enter size of second matrix: ', 'Enter second matrix:')
+        if mat1.shape != mat2.shape:
+            raise ShapeException('The operation cannot be performed.')
+        return mat1 + mat2
+
+    def mat_times_num(self):
+        mat = self.matrix_input('Enter size of matrix: ', 'Enter matrix:')
+        num = self.float_input('Enter constant: ')
+        return mat * num
+
+    def mat_times_mat(self):
+        mat1 = self.matrix_input('Enter size of first matrix: ', 'Enter first matrix:')
+        mat2 = self.matrix_input('Enter size of second matrix: ', 'Enter second matrix:')
+        if mat1.shape[1] != mat2.shape[0]:
+            raise ShapeException('The operation cannot be performed.\n')
+        return mat1 @ mat2
+
+    @staticmethod
+    def exit():
+        sys.exit()
+
+    def matrix_input(self, prompt1: str, prompt2: str) -> np.ndarray:
         try:
-            self.n_rows, self.n_columns = [int(_) for _ in input().split()]
-            return MyArray([[int(val) for val in input().split()] for _ in range(self.n_rows)])
+            n_rows, n_columns = [int(_) for _ in input(prompt1).split()]
+            print(prompt2)
+            arr = MyArray([[float(val) for val in input().split()] for _ in range(n_rows)])
+            if arr.shape != (n_rows, n_columns):
+                raise ValueError
         except ValueError:
             self.error()
+        else:
+            return arr
 
-    def int_input(self) -> int:
+    def float_input(self, prompt: str) -> float:
         try:
-            return int(input())
+            return float(input(prompt))
         except ValueError:
             self.error()
 
@@ -39,10 +87,7 @@ class NumericMatrixProcessor:
 
 def main() -> None:
     processor = NumericMatrixProcessor()
-    matrix = processor.matrix_input()
-    num = processor.int_input()
-    result = num * matrix
-    print(result)
+    processor.main_menu()
 
 
 if __name__ == '__main__':
