ccaf9e0bdf5b42d79e5a180b07a0c7b961741fae
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-09-26 23:56:07 +0700

    Add files via upload
    stage 6/6

--- convoy.py
+++ convoy.py
@@ -10,7 +10,7 @@ from lxml import etree
 class Convoy:
     def __init__(self) -> None:
         self.dispatch: dict[str, callable] = {
-            'to_json': self.s3db_to_json,
+            'to_json_xml': self.s3db_to_json_xml,
             'to_s3db': self.checked_to_s3db,
             'to_checked': self.csv_to_checked,
             'to_csv': self.xlsx_to_csv
@@ -24,7 +24,7 @@ class Convoy:
                 break
 
         if self.file_name.endswith('.s3db'):
-            operation = 'to_json'
+            operation = 'to_json_xml'
         elif self.file_name.endswith('[checked].csv'):
             operation = 'to_s3db'
         elif self.file_name.endswith('csv'):
@@ -34,38 +34,29 @@ class Convoy:
 
         self.process_data(operation)
 
-    def process_data(self, operation_type):
-        if operation_type in self.dispatch:
-            return self.dispatch[operation_type]()
+    def process_data(self, operation):
+        if operation in self.dispatch:
+            return self.dispatch[operation](self.file_name)
         else:
             return "Invalid input type"
 
-    def xlsx_to_csv(self, xlsx_file: str = None) -> None:
-        if xlsx_file is None:
-            xlsx_file = self.file_name
-
-        sheet_name: str = 'Vehicles'
-        df: pd.DataFrame = pd.read_excel(xlsx_file, sheet_name=sheet_name, dtype=str)
+    def xlsx_to_csv(self, xlsx_file: str) -> None:
+        df: pd.DataFrame = pd.read_excel(xlsx_file, sheet_name='Vehicles', dtype=str)
         csv_file: str = xlsx_file[:-4] + 'csv'
         df.to_csv(path_or_buf=csv_file, index=False, header=True)
         lines_num: int = df.shape[0]
-        report: str = ('1 line was' if lines_num == 1 else f'{lines_num} lines were') + f' added to {csv_file}'
-        print(report)
+        print(('1 line was' if lines_num == 1 else f'{lines_num} lines were') + f' added to {csv_file}')
         return self.csv_to_checked(csv_file)
 
-    def csv_to_checked(self, csv_file: str = None) -> None:
-        if csv_file is None:
-            csv_file = self.file_name
-
+    def csv_to_checked(self, csv_file: str) -> None:
         df: pd.DataFrame = pd.read_csv(filepath_or_buffer=csv_file, header='infer')
         checked_csv_file: str = csv_file[:-4] + '[CHECKED].csv'
 
         counter: int = 0
-        pattern: str = r'\D*(\d*).*'
         for row in range(df.shape[0]):
             for column in range(df.shape[1]):
                 data = df.iloc[row, column]
-                re_match = re.match(pattern=pattern, string=data)
+                re_match = re.match(pattern=r'\D*(\d*).*', string=data)
                 if re_match:
                     fixed_data = re_match.group(1)
                     if data != fixed_data:
@@ -75,78 +66,97 @@ class Convoy:
                     df.iloc[row, column] = np.nan
 
         df.to_csv(path_or_buf=checked_csv_file, index=False, header=True)
-        report: str = ('1 cell was' if counter == 1 else f'{counter} cells were') + f' corrected in {checked_csv_file}'
-        print(report)
+        print(('1 cell was' if counter == 1 else f'{counter} cells were') + f' corrected in {checked_csv_file}')
         return self.checked_to_s3db(checked_csv_file)
 
-    def checked_to_s3db(self, checked_csv_file: str = None) -> None:
-        if checked_csv_file is None:
-            checked_csv_file = self.file_name
-
+    def checked_to_s3db(self, checked_csv_file: str) -> None:
         df: pd.DataFrame = pd.read_csv(filepath_or_buffer=checked_csv_file, header='infer')
+        df = self.add_score(df)
+
         db_file: str = checked_csv_file[:-13] + '.s3db'
         conn = sqlite3.connect(db_file)
         cursor = conn.cursor()
         columns = df.columns.tolist()
-        table_name = 'convoy'
-        create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns[0]} INTEGER PRIMARY KEY, " + ", ".join(
+        create_table_query = f"CREATE TABLE IF NOT EXISTS convoy ({columns[0]} INTEGER PRIMARY KEY, " + ", ".join(
             [f"{col} INTEGER NOT NULL" for col in columns[1:]]) + ");"
         cursor.execute(create_table_query)
-        # result = df.to_sql(name=table_name, con=conn, if_exists='append', index=False)
-        insert_query: str = f'INSERT or REPLACE INTO {table_name} VALUES (?, ?, ?, ?)'
+        insert_query: str = f'INSERT or REPLACE INTO convoy VALUES (?, ?, ?, ?, ?)'
         cursor.executemany(insert_query, df.values.tolist())
         result = cursor.rowcount
 
         conn.commit()
+        cursor.close()
         conn.close()
 
-        report: str = ('1 record was' if result == 1 else f'{result} records were') + f' inserted into {db_file}'
-        print(report)
-        return self.s3db_to_json(db_file)
+        print(('1 record was' if result == 1 else f'{result} records were') + f' inserted into {db_file}')
+        return self.s3db_to_json_xml(db_file, df)
 
-    def s3db_to_json(self, db_file: str = None) -> None:
-        if db_file is None:
-            db_file = self.file_name
+    @staticmethod
+    def add_score(df: pd.DataFrame) -> pd.DataFrame:
 
-        # Connect to the SQLite database
-        table_name: str = 'convoy'
-        row_name = 'vehicle'
+        def calculate_score(row) -> int:
+            score: int = 0
+            pitstops_num: float = (row['fuel_consumption'] * 450 / 100) / row['engine_capacity']
+            score += 0 if pitstops_num >= 2 else 1 if pitstops_num >= 1 else 2  # score for number of pitstops
+            fuel_burned: float = 450 / row['fuel_consumption']
+            score += 2 if fuel_burned <= 230 else 1  # score for fuel consumption
+            score += 2 if row['maximum_load'] >= 20 else 0  # score for capacity
+            return score
+
+        # Apply function to each row and create a new column
+        df['score'] = df.apply(calculate_score, axis=1)
+        return df
+
+    def s3db_to_json_xml(self, db_file: str, df: pd.DataFrame) -> None:
         json_file: str = db_file[:-4] + 'json'
         xml_file: str = db_file[:-4] + 'xml'
-        conn = sqlite3.connect(db_file)
-        cursor = conn.cursor()
 
-        # Fetch all rows from the table
-        select_query: str = f'SELECT * FROM {table_name}'
-        cursor.execute(select_query)
-        rows = cursor.fetchall()
-        count = len(rows)
+        # Convert DataFrame to dict
+        convoy_list = df.to_dict(orient='records')
+        list_for_json = []
+        list_for_xml = []
 
-        # Fetch column names
-        column_names = [description[0] for description in cursor.description]
+        for entry in convoy_list:  # Separate entries for JSON and XML
+            if entry['score'] > 3:
+                list_for_json.append(entry)
+            else:
+                list_for_xml.append(entry)
 
-        # Convert the rows into a list of dictionaries (JSON format)
-        data = [dict(zip(column_names, row)) for row in rows]
-        my_dict = {table_name: data}
+        for entry in list_for_json:  # Remove score
+            del entry['score']
+        for entry in list_for_xml:
+            del entry['score']
+        rows_json = len(list_for_json)
+        rows_xml = len(list_for_xml)
+        dict_for_json = {'convoy': list_for_json}
+        dict_for_xml = {'convoy': list_for_xml}
 
-        # Write to a JSON file
         with open(json_file, 'w') as file:
-            json.dump(my_dict, file, indent=4)
+            json.dump(dict_for_json, file, indent=4)
 
-        # Read data from the table into a pandas DataFrame
-        df = pd.read_sql_query(f"SELECT * FROM {table_name}", conn)
+        if rows_xml != 0:
+            # Create the root element
+            root = etree.Element("convoy")
+            # Create XML tree from dictionary
+            for vehicle in dict_for_xml['convoy']:
+                vehicle_elem = etree.SubElement(root, "vehicle")
+                for key, value in vehicle.items():
+                    child = etree.SubElement(vehicle_elem, key)
+                    child.text = str(value)
 
-        conn.close()
+            # Convert the XML tree to a byte string (pretty print for readability)
+            xml_data = etree.tostring(root, pretty_print=True, encoding='utf-8')
+            # Write the XML to a file
+            with open(xml_file, 'wb') as file:  # "wb" mode for writing bytes
+                file.write(xml_data)
+        else:
+            with open(xml_file, 'w') as file:
+                file.write('<convoy></convoy>')
 
-        # Convert DataFrame to XML and save to a file
-        df.to_xml(xml_file, index=False, root_name=table_name, row_name=row_name, pretty_print=True, xml_declaration=False)
-
-        report: str = ('1 vehicle was' if count == 1 else f'{count} vehicles were') + f' saved into {json_file}'
+        report: str = ('1 vehicle was' if rows_json == 1 else f'{rows_json} vehicles were') + f' saved into {json_file}'
         print(report)
-        report: str = ('1 vehicle was' if count == 1 else f'{count} vehicles were') + f' saved into {xml_file}'
+        report: str = ('1 vehicle was' if rows_xml == 1 else f'{rows_xml} vehicles were') + f' saved into {xml_file}'
         print(report)
-        # print(my_dict)
-        # print(df.to_xml(index=False, root_name=table_name, row_name=row_name, pretty_print=True, xml_declaration=False))
 
 
 def main():
