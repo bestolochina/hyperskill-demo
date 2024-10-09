695c54ed8005e99261f8d138eabacc9de169408b
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-10-10 02:37:38 +0700

    Add files via upload
    To do list - HyperSkill project

--- todolist.py
+++ todolist.py
@@ -36,7 +36,9 @@ def menu() -> callable:
         '1': ("1) Today's tasks", today_tasks),
         '2': ("2) Week's tasks", week_tasks),
         '3': ("3) All tasks", all_tasks),
-        '4': ("4) Add a task", add_task),
+        '4': ("4) Missed tasks", missed_tasks),
+        '5': ("5) Add a task", add_task),
+        '6': ("6) Delete a task", delete_task),
         '0': ("0) Exit", exit_menu),
     }
     for key in menu_dict:
@@ -83,6 +85,18 @@ def all_tasks():
     return
 
 
+def missed_tasks():
+    rows = session.query(Task).filter(Task.deadline < datetime.today().date()).order_by(Task.deadline).all()
+    print('Missed tasks:')
+    if not rows:
+        print('All tasks have been completed!')
+    else:
+        for num, row in enumerate(rows, start=1):
+            print(f'{num}. {row.task}. {row.deadline.day} {row.deadline.strftime('%b')}')
+    print()
+    return
+
+
 def add_task():
     task = input('Enter a task\n')
     deadline = datetime.strptime(input('Enter a deadline\n'), '%Y-%m-%d')
@@ -91,6 +105,27 @@ def add_task():
     session.commit()
     print('The task has been added!\n')
     return
+
+
+def delete_task():
+    rows = dict(enumerate(session.query(Task).order_by(Task.deadline).all(), start=1))
+    if not rows:
+        print('Nothing to delete')
+    else:
+        print('Choose the number of the task you want to delete:')
+        for num, row in rows.items():
+            print(f'{num}. {row.task}. {row.deadline.day} {row.deadline.strftime('%b')}')
+        num = int(input())
+        row_to_delete = rows[num]
+        id_to_delete = row_to_delete.id
+        task_to_delete = session.query(Task).filter_by(id=id_to_delete).one_or_none()
+        if task_to_delete:
+            session.delete(task_to_delete)
+            session.commit()
+            print('The task has been deleted!')
+        else:
+            print('Task not found.')  # just in case
+    print()
 
 
 def exit_menu():
