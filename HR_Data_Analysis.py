35886a4fe98441973a8ff6d2377d2a8aa4ff3d20
bestolochina <144358841+bestolochina@users.noreply.github.com>
2024-12-19 02:11:59 +0700

    Add files via upload
    stage 5/5

--- HR_Data_Analysis.py
+++ HR_Data_Analysis.py
@@ -5,19 +5,14 @@ import os
 
 
 # Configure logging with a format that includes the logger name
-logging.basicConfig( filename='HR_Data_Analysis.log',
-                     encoding='utf-8',
-                     level=logging.DEBUG,
-                     format='%(name)s - %(levelname)s - %(message)s')
-
+# logging.basicConfig( filename='HR_Data_Analysis.log',
+#                      encoding='utf-8',
+#                      level=logging.DEBUG,
+#                      format='%(name)s - %(levelname)s - %(message)s')
 # Create and configure loggers
-app_logger = logging.getLogger('app')
+# app_logger = logging.getLogger('app')
 # db_logger = logging.getLogger('app.database')
 # auth_logger = logging.getLogger('app.authentication')
-
-
-def count_bigger_5(series: pd.Series) -> int:
-    return (series > 5).sum()
 
 
 if __name__ == '__main__':
@@ -54,11 +49,6 @@ if __name__ == '__main__':
     B_office_data_df = pd.read_xml('../Data/B_office_data.xml')
     hr_data_df = pd.read_xml('../Data/hr_data.xml')
 
-    # # Check for null values in each column
-    # print(A_office_data_df.isnull().values.any())  # Returns True if any null values exist
-    # print(B_office_data_df.isnull().values.any())
-    # print(hr_data_df.isnull().values.any())
-
     new_a_index = [f'A{row}' for row in A_office_data_df['employee_office_id']]
     A_office_data_df.index = new_a_index
     new_b_index = [f'B{row}' for row in B_office_data_df.employee_office_id]
@@ -70,27 +60,47 @@ if __name__ == '__main__':
     AB_office_data_df = pd.concat([A_office_data_df, B_office_data_df])
 
     # Merging hr_data_df and AB_office_data_df
-    result_df = pd.merge(AB_office_data_df, hr_data_df,
-                         how='left',
-                         left_index=True,
-                         right_index=True,
-                         indicator=True)
+    df = pd.merge(AB_office_data_df, hr_data_df,
+                  how='left',
+                  left_index=True,
+                  right_index=True,
+                  indicator=True)
 
     # Filter rows where the indicator column shows data in both datasets
-    result_df = result_df[result_df['_merge'] == 'both']
+    df = df[df['_merge'] == 'both']
 
     # Drop 3 columns - 'employee_office_id', 'employee_id', '_merge'
-    result_df = result_df.drop(columns=['employee_office_id', 'employee_id', '_merge'])
+    df = df.drop(columns=['employee_office_id', 'employee_id', '_merge'])
 
     # Sort result_df by index
-    result_df = result_df.sort_index(axis=0, inplace=False)
+    df = df.sort_index(axis=0, inplace=False)
 
-    # Aggregation
-    result = result_df.groupby('left').agg({
-        'number_project': ['median', count_bigger_5],
-        'time_spend_company': ['mean', 'median'],
-        'Work_accident': 'mean',
-        'last_evaluation': ['mean', 'std']
-    }).round(2).to_dict()
+    # Use df.pivot_table() to generate the first pivot table: Department as index, left and salary as columns,
+    # average_monthly_hours as values. Output median values in the table.
+    table1 = df.pivot_table(index='Department',
+                            columns=['left', 'salary'],
+                            values='average_monthly_hours',
+                            aggfunc='median')
+    # For the currently employed: the median value of the working hours of high-salary employees is smaller
+    # than the hours of the medium-salary employees,
+    # OR: For the employees who left: the median value of working hours of low-salary employees is smaller
+    # than the hours of high-salary employees
+    table1 = table1.loc[
+        (table1[(1, 'high')] < table1[(1, 'medium')]) | (table1[(0, 'low')] < table1[(0, 'high')])
+    ].round(2)
+    # print(table1)
+    print(table1.to_dict())
 
-    print(result)
+    # Use df.pivot_table() to generate the second pivot table: time_spend_company as index, promotion_last_5years as
+    # column, satisfaction_level and last_evaluation as values. Output the min, max, and mean values in the table.
+    table2 = df.pivot_table(index='time_spend_company',
+                            columns='promotion_last_5years',
+                            values=['satisfaction_level', 'last_evaluation'],
+                            aggfunc=['min', 'max', 'mean'])
+    # Filter the table by the following rule: select only those rows where the previous mean evaluation score is
+    # higher for those without promotion than those who had
+    table2 = table2.loc[
+        table2[('mean', 'last_evaluation', 0)] > table2[('mean', 'last_evaluation', 1)]
+    ].round(2)
+    # print(table2)
+    print(table2.to_dict())
