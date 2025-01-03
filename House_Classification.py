88bf84e40caf5a45138b908a29291b3987da62a9
bestolochina <144358841+bestolochina@users.noreply.github.com>
2025-01-03 21:21:59 +0700

    Add files via upload
    House_Classification - HyperSkill project

--- House_Classification.py
+++ House_Classification.py
@@ -6,70 +6,94 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 from category_encoders import TargetEncoder
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 
 
-def one_hot_encoding():
+def one_hot_encoding(X_train_, X_test_, y_train_, y_test_):
     # Create the encoder and specify the drop='first' parameter to drop the first column, created by the encoder
     encoder = OneHotEncoder(drop='first')
 
     # Fit the encoder to the training data using three categorical columns: Zip_area, Zip_loc, Room
-    encoder.fit(X_train[['Zip_area', 'Zip_loc', 'Room']])
+    encoder.fit(X_train_[['Zip_area', 'Zip_loc', 'Room']])
 
     # Transform the training and the test datasets with the fitted encoder
-    X_train_transformed = pd.DataFrame(encoder.transform(X_train[['Zip_area', 'Zip_loc', 'Room']]).toarray(),
-                                       index=X_train.index)
-    X_test_transformed = pd.DataFrame(encoder.transform(X_test[['Zip_area', 'Zip_loc', 'Room']]).toarray(),
-                                      index=X_test.index)
+    X_train_transformed = pd.DataFrame(encoder.transform(X_train_[['Zip_area', 'Zip_loc', 'Room']]).toarray(),
+                                       index=X_train_.index)
+    X_test_transformed = pd.DataFrame(encoder.transform(X_test_[['Zip_area', 'Zip_loc', 'Room']]).toarray(),
+                                      index=X_test_.index)
 
     # Return the transformed data to the dataset
-    X_train_final = X_train[['Area', 'Lon', 'Lat']].join(X_train_transformed)
-    X_test_final = X_test[['Area', 'Lon', 'Lat']].join(X_test_transformed)
+    X_train_final = X_train_[['Area', 'Lon', 'Lat']].join(X_train_transformed)
+    X_test_final = X_test_[['Area', 'Lon', 'Lat']].join(X_test_transformed)
 
     # Convert column names to strings
-    return X_train_final.columns.astype(str), X_test_final.columns.astype(str)
+    X_train_final.columns = X_train_final.columns.astype(str)
+    X_test_final.columns = X_test_final.columns.astype(str)
+
+    # Predict the target values
+    y_predict_ = prediction(X_train_final, X_test_final, y_train_)
+    report = classification_report(y_test_, y_predict_, output_dict=True)
+    print(f'OneHotEncoder:{report['macro avg']['f1-score']:.2f}')
 
 
-def ordinal_encoding():
+def ordinal_encoding(X_train_, X_test_, y_train_, y_test_):
     # Create the encoder
     encoder = OrdinalEncoder()
 
     # Fit the encoder to the training data
-    encoder.fit(X_train[['Zip_area', 'Zip_loc', 'Room']])
+    encoder.fit(X_train_[['Zip_area', 'Zip_loc', 'Room']])
 
     # Transform the training and test datasets
-    X_train_transformed = pd.DataFrame(encoder.transform(X_train[['Zip_area', 'Zip_loc', 'Room']]),
-                                       index=X_train.index,
+    X_train_transformed = pd.DataFrame(encoder.transform(X_train_[['Zip_area', 'Zip_loc', 'Room']]),
+                                       index=X_train_.index,
                                        columns=['Zip_area', 'Zip_loc', 'Room'])
-    X_test_transformed = pd.DataFrame(encoder.transform(X_test[['Zip_area', 'Zip_loc', 'Room']]),
-                                      index=X_test.index,
+    X_test_transformed = pd.DataFrame(encoder.transform(X_test_[['Zip_area', 'Zip_loc', 'Room']]),
+                                      index=X_test_.index,
                                       columns=['Zip_area', 'Zip_loc', 'Room'])
 
     # Replace the original categorical columns with the transformed ones
-    X_train_final = X_train.drop(['Zip_area', 'Zip_loc', 'Room'], axis=1).join(X_train_transformed)
-    X_test_final = X_test.drop(['Zip_area', 'Zip_loc', 'Room'], axis=1).join(X_test_transformed)
+    X_train_final = X_train_.drop(['Zip_area', 'Zip_loc', 'Room'], axis=1).join(X_train_transformed)
+    X_test_final = X_test_.drop(['Zip_area', 'Zip_loc', 'Room'], axis=1).join(X_test_transformed)
 
-    # Return the transformed datasets
-    return X_train_final, X_test_final
+    # Predict the target values
+    y_predict_ = prediction(X_train_final, X_test_final, y_train_)
+    report = classification_report(y_test_, y_predict_, output_dict=True)
+    print(f'OrdinalEncoder:{report['macro avg']['f1-score']:.2f}')
 
 
-def target_encoding():
+def target_encoding(X_train_, X_test_, y_train_, y_test_):
     # Create the encoder
     encoder = TargetEncoder(cols=['Zip_area', 'Room', 'Zip_loc'])
 
     # Fit the encoder on training data
-    encoder.fit(X_train[['Zip_area', 'Zip_loc', 'Room']], y_train)
+    encoder.fit(X_train_[['Zip_area', 'Zip_loc', 'Room']], y_train)
 
     # Transform only the specified columns
-    X_train_transformed = encoder.transform(X_train[['Zip_area', 'Zip_loc', 'Room']])
-    X_test_transformed = encoder.transform(X_test[['Zip_area', 'Zip_loc', 'Room']])
+    X_train_transformed = encoder.transform(X_train_[['Zip_area', 'Zip_loc', 'Room']])
+    X_test_transformed = encoder.transform(X_test_[['Zip_area', 'Zip_loc', 'Room']])
 
     # Replace the original categorical columns with the transformed ones
-    X_train_final = X_train[['Area', 'Lon', 'Lat']].join(X_train_transformed[['Room', 'Zip_area', 'Zip_loc']])
-    X_test_final = X_test[['Area', 'Lon', 'Lat']].join(X_test_transformed[['Room', 'Zip_area', 'Zip_loc']])
+    X_train_final = X_train_[['Area', 'Lon', 'Lat']].join(X_train_transformed[['Room', 'Zip_area', 'Zip_loc']])
+    X_test_final = X_test_[['Area', 'Lon', 'Lat']].join(X_test_transformed[['Room', 'Zip_area', 'Zip_loc']])
 
-    # Return the transformed datasets
-    return X_train_final, X_test_final
+    # Predict the target values
+    y_predict_ = prediction(X_train_final, X_test_final, y_train_)
+    report = classification_report(y_test_, y_predict_, output_dict=True)
+    print(f'TargetEncoder:{report['macro avg']['f1-score']:.2f}')
+
+
+def prediction(X_train_final_, X_test_final_, y_train_):
+    # Use DecisionTreeClassifier from scikit-learn.
+    # Fit the model to the training data and predict the house prices on the test data
+    clf = DecisionTreeClassifier(criterion='entropy',
+                                 max_features=3,
+                                 splitter='best',
+                                 max_depth=6,
+                                 min_samples_split=4,
+                                 random_state=3)
+    clf.fit(X_train_final_, y_train_)
+    y_predict_ = clf.predict(X_test_final_)
+    return y_predict_
 
 
 if __name__ == '__main__':
@@ -109,23 +133,6 @@ if __name__ == '__main__':
                                                         stratify=X['Zip_loc'].values,
                                                         random_state=1)
 
-    X_train_final, X_test_final = target_encoding()
-
-    # print(X_train_final.columns == X_test_final.columns)
-
-    # Use DecisionTreeClassifier from scikit-learn.
-    # Fit the model to the training data and predict the house prices on the test data
-    clf = DecisionTreeClassifier(criterion='entropy',
-                                 max_features=3,
-                                 splitter='best',
-                                 max_depth=6,
-                                 min_samples_split=4,
-                                 random_state=3)
-    clf.fit(X_train_final, y_train)
-    y_pred = clf.predict(X_test_final)
-
-    # Evaluate the model's accuracy using the accuracy_score function from sklearn.metrics
-    accuracy = accuracy_score(y_test, y_pred)
-
-    # Print the accuracy value
-    print(accuracy)
+    one_hot_encoding(X_train, X_test, y_train, y_test)
+    ordinal_encoding(X_train, X_test, y_train, y_test)
+    target_encoding(X_train, X_test, y_train, y_test)
