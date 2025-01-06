8943d275538c7c63ee6f33b22e2d83c607377523
bestolochina <144358841+bestolochina@users.noreply.github.com>
2025-01-06 22:00:12 +0700

    Add files via upload
    stage 3/5

--- Data_Analysis_for_Hospitals.py
+++ Data_Analysis_for_Hospitals.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 
-def load_data():
+def load_data() -> list[pd.DataFrame]:
     # define the data directory and file names
     data_dir = 'test'
     files = ['general.csv', 'prenatal.csv', 'sports.csv']
@@ -17,11 +17,28 @@ def load_data():
     return dataframes_
 
 
+def merge_data(dataframes_: list[pd.DataFrame]) -> pd.DataFrame:
+    # Change the column names as in 'general.csv'
+    dataframes_[1].columns = dataframes_[0].columns
+    dataframes_[2].columns = dataframes_[0].columns
+
+    # Merge the DataFrames into one
+    result = pd.concat(dataframes_, ignore_index=True)
+
+    # Delete the Unnamed: 0 column
+    result = result.drop(columns=['Unnamed: 0'])
+
+    # Return the result
+    return result
+
+
 if __name__ == '__main__':
     pd.set_option('display.max_columns', 8)
 
     # get a list of dataframes
     dataframes = load_data()
 
-    for df in dataframes:
-        print(df.head(n=20))
+    # merge dataframes into one
+    hospital_data = merge_data(dataframes)
+
+    print(hospital_data.sample(n=20, random_state=30))
