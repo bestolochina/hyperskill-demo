bd2a09413b1781108b39635fd8eaa3a574d62649
bestolochina <144358841+bestolochina@users.noreply.github.com>
2025-01-08 20:29:40 +0700

    Add files via upload
    stage 4/5

--- Data_Analysis_for_Hospitals.py
+++ Data_Analysis_for_Hospitals.py
@@ -32,7 +32,34 @@ def merge_data(dataframes_: list[pd.DataFrame]) -> pd.DataFrame:
     return result
 
 
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+    # Delete all the empty rows
+    df = df.dropna(axis=0, how='all').copy()  # Use `.copy()` to avoid potential view-related issues
+
+    # Correct all the gender column values to 'f' and 'm'
+    df.loc[:, 'gender'] = df['gender'].map({'male': 'm', 'man': 'm', 'female': 'f', 'woman': 'f'})
+
+    # Replace NaN values in the gender column for 'prenatal' hospital with 'f'
+    df.loc[(df['hospital'] == 'prenatal') & (df['gender'].isna()), 'gender'] = 'f'
+
+    # Replace NaN values in other specified columns with 0
+    columns_to_fill = ['bmi', 'diagnosis', 'blood_test', 'ecg', 'ultrasound', 'mri', 'xray', 'children', 'months']
+    df.loc[:, columns_to_fill] = df[columns_to_fill].fillna(value=0)
+
+    return df
+
+
+def output_data(df: pd.DataFrame) -> None:
+    # Print shape of the resulting DataFrame like in example "Data shape: (442, 14)"
+    print(f'Data shape: {df.shape}')
+
+    # Print random 20 rows of the resulting DataFrame. For the reproducible output set random_state=30
+    # Tip: To complete the last step use pandas.DataFrame.sample(n=20, random_state=30).
+    print(df.sample(n=20, random_state=30))
+
+
 if __name__ == '__main__':
+    # Keep pd.set_option('display.max_columns', 8) in your code.
     pd.set_option('display.max_columns', 8)
 
     # get a list of dataframes
@@ -41,4 +68,8 @@ if __name__ == '__main__':
     # merge dataframes into one
     hospital_data = merge_data(dataframes)
 
-    print(hospital_data.sample(n=20, random_state=30))
+    # preprocess data
+    hospital_data = preprocess_data(hospital_data)
+
+    # output data
+    output_data(hospital_data)
