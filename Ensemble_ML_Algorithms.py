fbd52d7a89e5aac4a319581b48dc0cbdfc4185ff
bestolochina <144358841+bestolochina@users.noreply.github.com>
2025-03-24 21:16:42 +0700

    Add files via upload
    stage 3/5

--- Ensemble_ML_Algorithms.py
+++ Ensemble_ML_Algorithms.py
@@ -1,7 +1,13 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import f1_score
 
 random_state = np.random.RandomState(seed=29)
 
@@ -11,4 +17,26 @@ X = df.drop(columns=['Class'])
 y = le.fit_transform(df['Class'])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=random_state)
 
-print({"train": [X_train.shape, y_train.shape], "test": [X_test.shape, y_test.shape]})
+classifiers = {'sgd': SGDClassifier(random_state=random_state),
+               'dt': DecisionTreeClassifier(random_state=random_state),
+               'kn': KNeighborsClassifier(),
+               'sv': SVC(probability=True, random_state=random_state)}
+
+results = {}
+for clf_name, clf in classifiers.items():
+    pipe = Pipeline([
+        ("scaler", StandardScaler()),
+        ("clf", clf)
+      ])
+
+    pipe.fit(X_train, y_train)
+
+    y_train_pred = pipe.predict(X_train)
+    y_test_pred = pipe.predict(X_test)
+
+    train_f1 = round(f1_score(y_train, y_train_pred, average="macro", labels=[0, 1, 2, 3]), 3)
+    test_f1 = round(f1_score(y_test, y_test_pred, average="macro", labels=[0, 1, 2, 3]), 3)
+
+    results[clf_name] = {'f1_train': train_f1, 'f1_test': test_f1}
+
+print(results)
